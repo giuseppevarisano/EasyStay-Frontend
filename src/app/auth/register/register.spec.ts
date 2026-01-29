ed7ee0632618c4c3da58b9421a1c38070761b471
giuseppe.varisano <giuseppe.varisano@hrm.group>
2026-01-29 18:16:15 +0100

    fix test componente register

--- src/app/auth/register/register.spec.ts
+++ src/app/auth/register/register.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { Register } from './register';
 
 describe('Register', () => {
@@ -8,9 +9,13 @@ describe('Register', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Register]
+      imports: [Register],
+      providers: [
+        provideRouter([]),        // Per Router e ActivatedRoute
+        provideHttpClient()        // Per AuthService che usa HttpClient
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(Register);
     component = fixture.componentInstance;
