644fe02dc23a4cabaaf7f831eae1e97313266d0a
giuseppe.varisano <giuseppe.varisano@hrm.group>
2026-01-29 18:14:34 +0100

    fix test componente login

--- src/app/auth/login/login.spec.ts
+++ src/app/auth/login/login.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { Login } from './login';
 
 describe('Login', () => {
@@ -8,9 +9,13 @@ describe('Login', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Login]
+      imports: [Login],
+      providers: [
+        provideRouter([]),        // Per Router e ActivatedRoute
+        provideHttpClient()        // Per AuthService che usa HttpClient
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(Login);
     component = fixture.componentInstance;
